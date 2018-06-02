38327602f85fc99682f3ccde4ec858d206972bd5
朱松杰 <vurtne0511@gmail.com>
2018-06-02 17:18:49 +0800

    文档修改

--- src/app/components/progress/demos/circle.ts
+++ src/app/components/progress/demos/circle.ts
@@ -4,17 +4,11 @@ import { setInterval } from 'timers';
 @Component({
   selector: 'demo-progress-circle',
   template: `
-    <nt-progress-circle [value]="value" size="small" radius="50"></nt-progress-circle>
+    <nt-progress-circle [value]="value"></nt-progress-circle>
     <nt-progress-circle [value]="value" color="secondary"></nt-progress-circle>
     <nt-progress-circle [value]="value" color="success"></nt-progress-circle>
     <nt-progress-circle [value]="value" color="alert"></nt-progress-circle>
     <nt-progress-circle [value]="value" color="warning"></nt-progress-circle>
-
-    <nt-progress-circle [value]="value"></nt-progress-circle>
-    <nt-progress-circle [value]="value" size="tiny"></nt-progress-circle>
-    <nt-progress-circle [value]="value" size="small"></nt-progress-circle>
-    <nt-progress-circle [value]="value" size="medium"></nt-progress-circle>
-    <nt-progress-circle [value]="value" size="large"></nt-progress-circle>
   `
 })
 export class DemoProgressCircleComponent implements AfterContentInit {
