97d09b23f6efa87e0d97301965db80f6b92f293d
朱松杰 <vurtne0511@gmail.com>
2018-11-16 15:26:44 +0800

    创建文件

---
+++ src/libs/components/switch/switch.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SwitchComponent } from './switch.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SwitchComponent]
+})
+export class SwitchModule { }
