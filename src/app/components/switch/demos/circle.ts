0f7eb258b6cf20e38631885e3201ecb265111ad4
朱松杰 <vurtne0511@gmail.com>
2018-12-15 15:21:19 +0800

    switch 文档更新

--- src/app/components/switch/demos/circle.ts
+++ src/app/components/switch/demos/circle.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-import { NtCheckboxChange } from '@ng-tangram/components/checkbox';
-
 
 @Component({
   selector: 'demo-switch-circle',
   template: `
     <nt-switch size="tiny" circle></nt-switch><br>
     <nt-switch circle="true"></nt-switch><br>
-    <nt-switch size="large" [circle]="circle"></nt-switch>
-    <br>
-    <nt-checkbox [checked]="circle" (change)="onChange($event)">设置为圆形</nt-checkbox>
+    <nt-switch size="large" circle="false"></nt-switch>
   `
 })
-export class DemoSwitchCircleComponent {
-  circle = true;
-
-  onChange(event: NtCheckboxChange) {
-    this.circle = event.checked;
-  }
-}
+export class DemoSwitchCircleComponent { }
