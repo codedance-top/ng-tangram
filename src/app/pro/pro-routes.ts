af227d9f3dac64e2ffbb447fb679ff93e4d8dc33
vurtne0511 <vurtne0511@gmail.com>
2019-06-20 16:25:02 +0800

    缺少开发模式的 文档markdown编译工序

--- src/app/pro/pro-routes.ts
+++ src/app/pro/pro-routes.ts
@@ -1,19 +1,29 @@
 import { Routes } from "@angular/router";
 
-export const PRO_ROUTES_CONFIG = [
-  { name: 'context-menu', title: 'ContextMenu 右键菜单', module: 'ContextMenuDocumentModule' },
-  { name: 'drawer', title: 'Drawer 抽屉式弹出框', module: 'DrawerDocumentModule' },
-  { name: 'markdown-block', title: 'Markdown 块', module: 'MarkdownBlockDocumentModule' },
-  { name: 'markdown-editor', title: 'Markdown 编辑器', module: 'MarkdownEditorDocumentModule' },
-];
-
 export const PRO_ROUTES: Routes = [
-  { path: '', redirectTo: PRO_ROUTES_CONFIG[0].name, pathMatch: 'full' },
-  ...PRO_ROUTES_CONFIG.map(route => {
-    return {
-      path: route.name,
-      loadChildren: () => import(`./${route.name}/${route.name}.module`).then(mod => mod[route.module]),
-      data: { title: route.title }
-    };
-  })
+  {
+    path: '',
+    redirectTo: 'context-menu',
+    pathMatch: 'full'
+  },
+  {
+    path: 'context-menu',
+    data: { title: 'ContextMenu 右键菜单' },
+    loadChildren: () => import('./context-menu/context-menu.module').then(mod => mod.ContextMenuDocumentModule)
+  },
+  {
+    path: 'drawer',
+    data: { title: 'Drawer 抽屉式弹出框' },
+    loadChildren: () => import('./drawer/drawer.module').then(mod => mod.DrawerDocumentModule)
+  },
+  {
+    path: 'markdown-block',
+    data: { title: 'Markdown 块' },
+    loadChildren: () => import('./markdown-block/markdown-block.module').then(mod => mod.MarkdownBlockDocumentModule)
+  },
+  {
+    path: 'markdown-editor',
+    data: { title: 'Markdown 编辑器' },
+    loadChildren: () => import('./markdown-editor/markdown-editor.module').then(mod => mod.MarkdownEditorDocumentModule)
+  },
 ];
