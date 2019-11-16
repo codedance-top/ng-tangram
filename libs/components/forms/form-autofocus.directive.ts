cc61b2f8a3e9eac5f4e8fb37d55accd5ee8f088f
vurtne0511 <vurtne0511@gmail.com>
2019-11-16 20:46:19 +0800

    chore(form-field): 组件源代码目录变更 `src/libs/components` => `libs/components`

---
+++ libs/components/forms/form-autofocus.directive.ts
@@ -0,0 +1,50 @@
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import {
+  AfterContentInit,
+  ContentChildren,
+  Directive,
+  OnDestroy,
+  Optional,
+  QueryList
+} from '@angular/core';
+import { FormGroupDirective, NgForm } from '@angular/forms';
+
+import { NtFormFieldComponent } from './form-field.component';
+
+@Directive({
+  selector: 'form[ntFormAutofocus]'
+})
+export class NtFormAutofocusDirective implements AfterContentInit, OnDestroy {
+
+  private _destory = new Subject();
+
+  formContainer: NgForm | FormGroupDirective;
+
+  @ContentChildren(NtFormFieldComponent, { descendants: true }) fields: QueryList<NtFormFieldComponent>;
+
+  constructor(
+    @Optional() form: NgForm,
+    @Optional() formGroup: FormGroupDirective) {
+    this.formContainer = form || formGroup;
+  }
+
+  ngAfterContentInit() {
+    if (this.formContainer) {
+      this.formContainer.ngSubmit
+        .pipe(takeUntil(this._destory))
+        .subscribe(() => {
+          const field = this.fields.find(field => !!field.ngControl && !!field.ngControl.invalid);
+          if (field && field.control) {
+            field.control.focus();
+          }
+        });
+    }
+  }
+
+  ngOnDestroy() {
+    this._destory.next();
+    this._destory.complete();
+  }
+}
