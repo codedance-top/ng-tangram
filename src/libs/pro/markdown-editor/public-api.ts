921dd30de7a7cb1a6ca2b088b36c500fd13c726a
vurtne0511 <vurtne0511@gmail.com>
2019-11-16 17:20:06 +0800

    refactor(pro): 废弃 pro 包，contextmenu 组件移动至 components 包下，新建了 @ng-tangram/markdown 包

--- src/libs/pro/markdown-editor/public-api.ts
+++
@@ -1,3 +0,0 @@
-export * from './markdown-editor.module';
-export * from './markdown-editor.component';
-export * from './markdown-editor-icons';
--- src/libs/pro/public-api.ts
+++
@@ -1,2 +0,0 @@
-export * from '@ng-tangram/pro/markdown-block';
-export * from '@ng-tangram/pro/markdown-editor';
