be70a4bd202fb87cac94774741fe1de518ef266a
vurtne0511 <vurtne0511@gmail.com>
2019-11-16 20:55:02 +0800

    chore(tooltip): 组件源代码目录变更 `src/libs/components` => `libs/components`

---
+++ libs/components/tooltip/tooltip.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NtOverlayModule } from '@ng-tangram/components/core';
+
+import { NtTooltipComponent } from './tooltip.component';
+
+@NgModule({
+  imports: [CommonModule, NtOverlayModule],
+  exports: [NtTooltipComponent],
+  declarations: [NtTooltipComponent]
+})
+export class NtTooltipModule { }
