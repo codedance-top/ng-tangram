e6fa3317ea3f860681038660eb6a9c991a19a955
vurtne0511 <vurtne0511@gmail.com>
2019-11-19 18:26:25 +0800

    chore(docs): 更新 universal 启动的代码 2019-11-19

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,1 +1,15 @@
+import { enableProdMode } from '@angular/core';
+
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
 export { AppServerModule } from './app/app.server.module';
+
+// Express Engine
+export { ngExpressEngine } from '@nguniversal/express-engine';
+
+// Import module map for lazy loading
+export { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
