607d30749367c9b1b7e84e688bbe96adb24b5b63
vurtne0511 <vurtne0511@gmail.com>
2020-02-10 15:12:46 +0800

    chore(core): 优化 core 模块的导出方式

--- src/libs/components/core/file/index.ts
+++ src/libs/components/core/file/index.ts
@@ -1,7 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { NtFileSelectDirective } from './file-select.directive';
+import { NtFileSizePipe } from './file-size.pipe';
 
 export * from './file-categories';
 export * from './file-select-errors';
+export * from './file-utils';
 export * from './file-select.directive';
 export * from './file-size.pipe';
-export * from './file.module';
-export * from './file-utils';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [
+    NtFileSizePipe,
+    NtFileSelectDirective
+  ],
+  declarations: [
+    NtFileSizePipe,
+    NtFileSelectDirective
+  ]
+})
+export class NtFileModule { }
