4456356ce6d64da10e8c06c317fd85a8772c6417
Vurtne-Z <vurtne0511@gmail.com>
2020-11-24 11:34:27 +0800

    fix(datepicker): 修复 `NtDatePickerControl` 导出会报错的问题

--- src/libs/components/datepicker/public-api.ts
+++ src/libs/components/datepicker/public-api.ts
@@ -2,14 +2,16 @@ export * from './datepicker.module';
 export * from './datepicker.component';
 export * from './datepicker-errors';
 export * from './datepicker-icons';
+export * from './datepicker-control';
 export * from './datepicker-base';
 export * from './datepicker-input-base';
 export * from './date-range-parts.directive';
 export * from './date-range-picker.component';
-
-export * from './selections';
 export * from './calendar.component';
 export * from './month.component';
 export * from './multi-year.component';
 export * from './year.component';
+
+export * from './selections';
+
 
